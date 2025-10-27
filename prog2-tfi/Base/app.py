6f4920f56e3b55a90724496c322b3e9dfa295e04
Sergio Ortega <sergioortegadev@gmail.com>
2025-10-27 19:53:55 -0300

    interfaz gráfica en app ok

--- prog2-tfi/Base/app.py
+++ prog2-tfi/Base/app.py
@@ -1,17 +1,33 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import servicio_concesionarias
 import servicio_clientes
 import servicio_vehiculos
 import time
+import datetime
 
 app = Flask(__name__)
-
+SERVER_START_TIME = time.time() # tiempo inicio server
 
 @app.route("/", methods=["GET"])
-def server_running():
+def index():
     print(f' -> Indicador de tráfico. Time: {time.time()} | Tráfico entrante desde la IP: {request.remote_addr}')
+    titulo = "Mi App de Concesionarias"
+    # 1. Calcular el tiempo transcurrido
+    tiempo_transcurrido_segundos = time.time() - SERVER_START_TIME
+    
+    # 2. Formatear el tiempo a Horas, Minutos y Segundos
+    # Usamos timedelta para un manejo fácil de la conversión de segundos a H:M:S
+    td = datetime.timedelta(seconds=int(tiempo_transcurrido_segundos))
+    
+    # Formato simple (H:MM:SS)
+    # Por ejemplo, '0:05:30' o '1:10:00'
+    tiempo_funcionamiento_formato = str(td) 
 
-    return f"<pre>---------------------------------- \n  ----  Server funcionando   ---\n----------------------------------</pre>", 200
+    # 3. Renderizar la plantilla, pasando la variable
+    return render_template(
+        'index.html', 
+        tiempo_funcionamiento=tiempo_funcionamiento_formato, page_title=titulo, server_status="Funciona!"  # <- Variable que usaremos en el HTML
+    )
 
 @app.route("/concesionarias/<concesionaria_id>/", methods=["GET"])
 def obtener_concesionaria(concesionaria_id):
--- prog2-tfi/Readme_Ubuntu_exe.txt
+++ prog2-tfi/Readme_Ubuntu_exe.txt
@@ -1,7 +1,7 @@
 -----------------------------------------------
 En UBUNTU - Primero ejecutar el entorno virtual
 -----------------------------------------------
-
+(entrar en /Base)
 
 python3 -m venv venv
 
